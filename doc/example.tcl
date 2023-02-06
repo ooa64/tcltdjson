58259c833315d2732a41b76644ffb93bb35e491d
ooa64 <ooa64@ua.fm>
2023-02-06 11:14:30 +0200

    updated example

--- doc/example.tcl
+++ doc/example.tcl
@@ -12,7 +12,13 @@ proc json2dict {json} {
     ]
 }
 
-# setting TDLib log verbosity level to 1 (errors)
+proc input {prompt} {
+    puts -nonewline stdout $prompt
+    flush stdout
+    string trim [gets stdin]
+}
+
+# setting TDLib log verbosity level to 1 (errors} $x]
 puts [td_execute {{"@type": "setLogVerbosityLevel", "new_verbosity_level": 1, "@extra": 1.01234}}]
 
 # create client
@@ -26,35 +32,78 @@ td_send $client_id {{"@type": "getOption", "name": "version", "@extra": 1.01234}
 
 # main events cycle
 while {true} {
-   set json [td_receive 1.0]
-   puts JSON:$json
+    set json [td_receive 1.0]
+    puts <$json>
 
-   set event [json2dict $json]
-   puts DICT:$event
+    set event [json2dict $json]
+    if {[dict exists $event "@type"] \
+            && [dict get $event "@type"] eq "updateAuthorizationState" \
+            && [dict exists $event "authorization_state" "@type"]} {
 
-   if {[dict exists $event "@type"] && [dict get $event "@type"] eq "updateAuthorizationState"} {
-       set auth_state_type [dict get $event "authorization_state" "@type"]
+        switch -- [dict get $event "authorization_state" "@type"] {
 
-       # if client is closed, we need to destroy it and create new client
-       if {$auth_state_type eq "authorizationStateClosed"} {
-           break
-       }
+            "authorizationStateClosed" {
+                # if client is closed, we need to destroy it and create new client
+                break
+            }
 
-       # set TDLib parameters
-       # you MUST obtain your own api_id and api_hash at https://my.telegram.org
-       # and use them in the setTdlibParameters call
-       if {$auth_state_type eq "authorizationStateWaitTdlibParameters"} {
-            td_send $client_id {{"@type": "setTdlibParameters",
-                 "database_directory": "tdlib",
-                 "use_message_database": 1,
-                 "use_secret_chats": 1,
-                 "api_id": 94575,
-                 "api_hash": "a3406de8d171bb422bb6ddf3bbd800e2",
-                 "system_language_code": "en",
-                 "device_model": "Desktop",
-                 "application_version": "1.0",
-                 "enable_storage_optimizer": 1}}
-       }
-   }
-   update idle
+            "authorizationStateWaitTdlibParameters" {
+                # set TDLib parameters
+                # you MUST obtain your own api_id and api_hash at https://my.telegram.org
+                # and use them in the setTdlibParameters call
+                td_send $client_id {{"@type": "setTdlibParameters",
+                     "database_directory": "tdlib",
+                     "use_message_database": 1,
+                     "use_secret_chats": 1,
+                     "api_id": 94575,
+                     "api_hash": "a3406de8d171bb422bb6ddf3bbd800e2",
+                     "system_language_code": "en",
+                     "device_model": "Desktop",
+                     "application_version": "1.0",
+                     "enable_storage_optimizer": 1}}
+            }
+
+            "authorizationStateWaitPhoneNumber" {
+                # enter phone number to log in
+                set phone_number [input "Please enter your phone number:  "]
+                td_send $client_id [format {{"@type": "setAuthenticationPhoneNumber", "phone_number": "%s"}} $phone_number]
+            }
+
+            "authorizationStateWaitEmailAddress" {
+                # enter email address to log in
+                set email_address [input "Please enter your email address:  "]
+                td_send $client_id [format {{"@type": "setAuthenticationEmailAddress", "email_address": "%s"}} $email_address]
+            }
+
+            "authorizationStateWaitEmailCode" {
+                # wait for email authorization code
+                set code [input "Please enter the email authentication code you received:  "]
+                td_send $client_id [format {{"@type": "emailAddressAuthenticationCode", "code" : "%s"}} $code]
+            }
+
+            "authorizationStateWaitCode" {
+                # wait for authorization code
+                set code [input "Please enter the authentication code you received:  "]
+                td_send $client_id [format {{"@type": "checkAuthenticationCode", "code": "%s"}} $code]
+            }
+
+            "authorizationStateWaitRegistration" {
+                # wait for first and last name for new users
+                set first_name [input "Please enter your first name:  "]
+                set last_name [input "Please enter your last name:  "]
+                td_send $client_id [format {{"@type": "registerUser", "first_name": "%s", "last_name": "%s"}} $first_name $last_name]
+            }
+
+            "authorizationStateWaitPassword" {
+                # wait for password if present
+                set password [input "Please enter your password:  "]
+                td_send $client_id [format {{"@type": "checkAuthenticationPassword", "password": "%s"}} $password]
+            }
+
+            default {
+                continue
+            }
+        }
+    }
+    update
 }
